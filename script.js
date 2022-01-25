c14c57b4159683956418e63375fa000ad04101fe
dhimaseka <dimaseka83@gmail.com>
2022-01-25 14:34:01 +0700

    push

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
-class Mail {
-    constructor() {
-        this.from = 'pengirim@dicoding';
-        this.contacts = [];
-        this.yourOtherProperty = 'the values';
-    }
 
+class Mail {
+    constructor(author){
+        this.from = author;
+        this._contacts = [];
+    }
     sendMessage(msg, to){
         console.log(`you send: ${msg} to ${to} from ${this.from}`);
-        this.contacts.push(to);
+        this._contacts.push(to);
+    }
+    showAllContacts(){
+        return this._contacts;
     }
 }
 
-const mail = new Mail();
-mail.sendMessage('Hallo', 'dimaseka83@gmail.com');
+const mail1 = new Mail("Dhimas");
+mail1.sendMessage("Hallo", "Eka");
+console.log(mail1.showAllContacts());
+
+class WhatsApp extends Mail {
+    constructor(username, isBussinessAccount, phone) {
+        super(phone);
+        this.username = username;
+        this.isBussinessAccount = isBussinessAccount;
+    }
+    
+    myProfile(){
+        return `my name ${this.username}, is ${this.isBussinessAccount ? 'Bussiness' : 'Personal'}`;
+    }
+}
+
+const wa1 = new WhatsApp('sinko', true, 0895647378);
+console.log(wa1.myProfile);
