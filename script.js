de45b4a9333cf8ebeaa89885f33b28a9b6ff4048
dhimas <dimaseka83@gmail.com>
2022-01-26 21:09:11 +0700

    push

--- script.js
+++ script.js
@@ -1,55 +1,18 @@
-/**
- * TODO:
- * 1. Buatlah class bernama Animal dengan ketentuan:
- *    - Memiliki properti:
- *      - name: string
- *      - age: int
- *      - isMammal: boolean
- *    - Memiliki constructor untuk menginisialisasi properti:
- *      - name
- *      - age
- *      - isMammal
- * 2. Buatlah class bernama Rabbit dengan ketentuan:
- *    - Merupakan turunan dari class Animal
- *    - Memiliki method:
- *      - eat yang mengembalikan nilai string "${this.name} sedang makan!"
- *    - Ketika diinstansiasi, properti isMammal harus bernilai true
- * 3. Buatlah class bernama Eagle dengan ketentuan:
- *    - Merupakan turunan dari class Animal
- *    - Memiliki method:
- *      - fly yang mengembalikan nilai string "${this.name} sedang terbang!"
- *    - Ketika diinstansiasi, properti isMammal harus bernilai false
- * 4. Buatlah instance dari class Rabbit bernama "myRabbit" dengan ketentuan:
- *    - properti name bernilai: "Labi"
- *    - properti age bernilai: 2
- * 5. Buatlah instance dari class Eagle bernama "myEagle" dengan ketentuan:
- *    - properti name bernilai: "Elo"
- *    - properti age bernilai: 4
- */
+//  Cara Lama
+const names = ['Harry', 'Ron', 'Jeff', 'Thomas'];
 
+const newNamesWithExcMark = [];
 
-// TODO
-class Animal {
-    constructor(name, age, isMammal) {
-        this.name = name;
-        this.age = age;
-        this.isMammal = isMammal;
-    }
+for (let i = 0; i < names.length; i++) {
+  newNamesWithExcMark.push(`${names[i]}`);
 }
 
-class Rabbit extends Animal{
-    constructor(name){
-      this.name = name;
+console.log(newNamesWithExcMark);
 
-    }
-}
+//  Cara Baru
 
+// const names = ['Harry', 'Ron', 'Jeff', 'Thomas'];
 
+const newNamesWithExcMarks = names.map((name) => `${name}!`);
 
-/**
- * Jangan hapus kode di bawah ini
- */
-
-module.exports = {
-  Animal, Rabbit, Eagle, myRabbit, myEagle,
-};
+console.log(newNamesWithExcMarks);
