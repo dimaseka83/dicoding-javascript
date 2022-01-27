f9a1ad382ed3ff9dba5623b007a5de10067528ae
dhimas <dimaseka83@gmail.com>
2022-01-27 22:00:06 +0700

    push

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
+let json = '{ "name": "Yoda","age": 20 }';
+
 try {
-  console.log("Awal blok try");
+  let user = JSON.parse(json);
+
+  if (!user.name) {
+    throw new SyntaxError("'name' is required")
+  }
+
   errorCode;
-  console.log("Akhir blok try");
+
+  console.log(user.name);
+  console.log(user.age);
 } catch (error) {
-    console.log(error.name);
+  if (error instanceof SyntaxError) {
+    console.log(`JSON Error: ${error.message}`);  
+  }else if (error instanceof ReferenceError) {
     console.log(error.message);
+  } else {
     console.log(error.stack);
-} finally {
-  console.log("Akan tetap dieksekusi");
+  }
 }
