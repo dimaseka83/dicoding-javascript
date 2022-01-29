8ab9a85e02cf129826f4ef2cf3c6e4118b3bf7e0
dhimas <dimaseka83@gmail.com>
2022-01-29 13:32:35 +0700

    push

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const state = {
     coffeeBeans: 250,
     water: 1000,
   },
-  isCoffeeMachineBusy: true,
+  isCoffeeMachineBusy: false,
 }
 
 const checkAvailability = () => {
@@ -40,17 +40,41 @@ const brewCoffee = () => {
   })
 }
 
+const boilWater = () => {
+  return new Promise((resolve, reject) => {
+    console.log("Memanaskan air...");
+    setTimeout(() => {
+      resolve("Air panas sudah siap!");
+    }, 2000);
+  })
+}
+
+const grindCoffeeBeans = () => {
+  return new Promise((resolve, reject) => {
+    console.log("Menggiling biji kopi...");
+    setTimeout(() => {
+      resolve("Kopi sudah siap!")
+    }, 1000);
+  })
+}
+
 function makeEspresso() {
     checkAvailability().then((value) => {
       console.log(value);
       return checkStock();
     }).then((value) => {
       console.log(value);
+      const promises = [boilWater(), grindCoffeeBeans()];
+      return Promise.all(promises);
+    }).then((value) => {
+      console.log(value);
       return brewCoffee();
     }).then((value) => {
       console.log(value);
+      state.isCoffeeMachineBusy = false;
     }).catch((rejectedReason) => {
       console.log(rejectedReason);
+      state.isCoffeeMachineBusy = false;
     })
 }
 
