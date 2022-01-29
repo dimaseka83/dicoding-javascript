5785d4a6eb53331e278c71bb8758e8bbc4865073
dhimas <dimaseka83@gmail.com>
2022-01-29 16:57:07 +0700

    push

--- script.js
+++ script.js
@@ -1,23 +1,78 @@
-const getCoffee = () => {
+const state = {
+  stock: {
+    coffeeBeans: 250,
+    water: 1000,
+  },
+  isCoffeeMachineBusy: false,
+}
+
+const checkAvailability = () => {
   return new Promise((resolve, reject) => {
-    const seeds = 1;
     setTimeout(() => {
-      if (seeds >= 10) {
-        resolve("Kopi didapatkan!");
+      if (!state.isCoffeeMachineBusy) {
+        resolve("Mesin kopi sudah siap digunakan.");
       } else {
-        reject("Biji kopi habis!");
+        reject("Maaf, mesin sedang sibuk.");
       }
     }, 1000);
   })
 }
 
-async function makeCoffee() {
-  try {
-    const coffe = await getCoffee();
-    console.log(coffe);
-  } catch (error) {
-    console.log(error);
-  }
+const checkStock = () => {
+  return new Promise((resolve, reject) => {
+    state.isCoffeeMachineBusy = true;
+    setTimeout(() => {
+      if (state.stock.coffeeBeans >= 16 && state.stock.water >= 250) {
+        resolve(
+          "Stok cukup bisa membuat kopi.",
+          state.stock.coffeeBeans -= 16,
+          state.stock.water -= 250
+        );
+
+      } else {
+        reject("Stok tidak cukup!");
+      }
+    }, 1500);
+  })
 }
 
-makeCoffee();
+const boilWater = () => {
+  return new Promise((resolve, reject) => {
+    console.log("Memanaskan air...");
+    setTimeout(() => {
+      resolve("Air panas sudah siap!");
+    }, 2000);
+  })
+}
+
+const grindCoffeeBeans = () => {
+  return new Promise((resolve, reject) => {
+    console.log("Menggiling biji kopi...");
+    setTimeout(() => {
+      resolve("Kopi sudah siap!");
+    }, 1000);
+  })
+}
+
+const brewCoffe = () => {
+  console.log("Membuatkan kopi Anda....");
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Kopi sudah siap!")
+    }, 2000);
+  })
+}
+
+async function makeEspresso() {
+  try {
+    await checkAvailability();
+    await checkStock();
+    await Promise.all([boilWater(), grindCoffeeBeans()]);
+    const coffee = await brewCoffe();
+    console.log(coffee);
+  } catch (rejectedReason) {
+    console.log(rejectedReason);
+  }
+}
+makeEspresso();
+console.log(state);
